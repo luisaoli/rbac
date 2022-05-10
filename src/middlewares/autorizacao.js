dfe27385128dbf503350b00a5fe2ec95833b5663
luisaoli <luisaoliveira.e@gmail.com>
2022-05-10 14:39:39 -0300

    controle de acesso por rbac

--- src/middlewares/autorizacao.js
+++ src/middlewares/autorizacao.js
@@ -1,8 +1,16 @@
-module.exports = (cargosObrigatorios) => (req, res, next) => {
+const controle = require('../controleDeAcesso');
 
-    if (cargosObrigatorios.indexOf(req.user.cargo) === -1) {
+module.exports = (entidade, acao) => (req, res, next) => {
+    const permissoesDoCargo = controle.can(req.user.cargo);
+    const permissao = permissoesDoCargo[acao](entidade)
+
+    if (permissao.granted === false) {
         res.status(403).end();
         return;
+    }
+
+    requisicao.acesso = {
+        atributos: permissao.attributes
     }
 
     next();
